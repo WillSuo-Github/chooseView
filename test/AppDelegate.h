b8b68324b7bdbf7008daada89b90e13a85f44923
李岩柯 <ws@wsdemacbook-pro.local>
2016-07-11 00:26:33 -0700

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ws on 16/7/11.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
